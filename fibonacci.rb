e1bb8ec4a2b1601729b33227095c4241aeb8e877
kthong3 <kthongsakounh@gmail.com>
2018-06-28 13:18:22 -0500

    calculate fibonacci sequence iteratively

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,2 +1,16 @@
-def fibonacci(number)
+def calculate_fibonacci_number(number)
+  if number == 0
+    return 0
+  elsif number == 1
+    return 1
+  end
+
+  i = 1
+  fib = [0, 1]
+  until i == number
+    fib << (fib[-1] + fib[-2])
+    i += 1
+  end
+
+  fib.last
 end
--- fibonacci_spec.rb
+++ fibonacci_spec.rb
@@ -1,5 +1,43 @@
 require_relative 'fibonacci'
 
-describe '#fibonacci' do
+describe 'nth_fibonacci_number' do
+  it 'calculates the 0th fibonacci number' do
+    calculated_number = calculate_fibonacci_number(0)
+    expect(calculated_number).to eq 0
+  end
 
+  it 'calculates the 1st fibonacci number' do
+    calculated_number = calculate_fibonacci_number(1)
+    expect(calculated_number).to eq 1
+  end
+
+  it 'calculates the 2nd fibonacci number' do
+    calculated_number = calculate_fibonacci_number(2)
+    expect(calculated_number).to eq 1
+  end
+
+  it 'calculates the 3rd fibonacci number' do
+    calculated_number = calculate_fibonacci_number(3)
+    expect(calculated_number).to eq 2
+  end
+
+  it 'calculates the 4th fibonacci number' do
+    calculated_number = calculate_fibonacci_number(4)
+    expect(calculated_number).to eq 3
+  end
+
+  it 'calculates the 5th fibonacci number' do
+    calculated_number = calculate_fibonacci_number(5)
+    expect(calculated_number).to eq 5
+  end
+
+  it 'calculates the 6th fibonacci number' do
+    calculated_number = calculate_fibonacci_number(6)
+    expect(calculated_number).to eq 8
+  end
+
+  it 'calculates the 17th fibonacci number' do
+    calculated_number = calculate_fibonacci_number(17)
+    expect(calculated_number).to eq 1597
+  end
 end
